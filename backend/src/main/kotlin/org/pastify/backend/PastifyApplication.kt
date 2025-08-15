fefaec04bffa1f38e1d6d522ec15b33b55392a31
ya-ilya <ilya.tseplaev@gmail.com>
2025-08-16 02:23:51 +0300

    Remove CommandLineRunner implementation and user creation logic from PastifyApplication

--- backend/src/main/kotlin/org/pastify/backend/PastifyApplication.kt
+++ backend/src/main/kotlin/org/pastify/backend/PastifyApplication.kt
@@ -1,34 +1,12 @@
 package org.pastify.backend
 
-import org.pastify.backend.services.UserService
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = ["org.pastify.backend"])
-class PastifyApplication(
-    private val userService: UserService
-) : CommandLineRunner {
-    override fun run(vararg args: String) {
-        if (userService.findUserByUsername("Ilya").isPresent) {
-            return
-        }
-
-        userService.createUser(
-            "Ilya",
-            "ilya@mail.com",
-            "password"
-        )
-
-        userService.createUser(
-            "Pavel",
-            "pavel@mail.com",
-            "password"
-        )
-    }
-}
+class PastifyApplication
 
 fun main(args: Array<String>) {
     runApplication<PastifyApplication>(*args)
